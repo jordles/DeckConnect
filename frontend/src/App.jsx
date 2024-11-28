77677517320e41062ff843d973833c8e47da90d0
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-11-27 20:51:22 -0700

    Created nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import FeedPage from './pages/FeedPage'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/feed" element={<FeedPage />} />
       </Routes>
+      <NavBar />
     </>
   )
 }
