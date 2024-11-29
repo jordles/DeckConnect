0c31166034149a7146b1f2015847e90540a3eee1
Jacky Yang <jackyyang1670@gmail.com>
2024-11-29 02:52:59 -0800

    Finished Deck Page

---
+++ frontend/src/components/DeckList.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './DeckList.css'
+import GoogleIcon from './GoogleIcon'
+import { useState } from 'react'
+
+const DeckList = ({name, difficulty}) => {
+  const [openOptions, setOpenOptions] = useState(false);
+  
+  const handleOptions = () => {
+    setOpenOptions(!openOptions);
+  }
+
+  return (
+    <div className="deck-list">
+      <li><span>{name}</span><span>{difficulty}</span></li>
+      <div className = "progress"></div>
+      <GoogleIcon icon = "settings" onClick={handleOptions}/>
+      <div className={`options ${openOptions ? 'open' : ''}`}>
+        <GoogleIcon icon = "edit" onClick={() => console.log("edit")}/>
+        <GoogleIcon icon = "delete" onClick={() => console.log("delete")}/>
+      </div>
+    </div>
+  )
+}
+
+export default DeckList
--- frontend/src/pages/DecksPage.jsx
+++ frontend/src/pages/DecksPage.jsx
@@ -1,15 +1,38 @@
 import BackgroundAnim from "../components/BackgroundAnim"
 import './DecksPage.css'  
-
+import DeckList from "../components/DeckList"
+import GoogleIcon from "../components/GoogleIcon"
 const DecksPage = () => {
   return (
     <BackgroundAnim color="green" outlineColor="yellow" title="Decks">
       <div className="decks-page">
-        <header>
-        </header>
-        <main>
-          
-        </main>
+        <div className = "left">
+          <div>Custom Decks <GoogleIcon icon = "add" size="32px"/></div>
+          <div>Premade Decks <GoogleIcon icon = "add" size="32px"/></div>
+          <div>Import Deck <GoogleIcon icon = "add" size="32px"/></div>
+          <div>Create Deck <GoogleIcon icon = "add" size="32px"/></div>
+          <div>Search Deck <GoogleIcon icon = "search" size="32px"/></div>
+        </div>
+        <div className = "right">
+          <header>
+            <div className="deck-selection">All Decks</div>
+          </header>
+          <main>
+            <ul style = {{listStyleType: 'none'}}>
+              <DeckList name="Deck 1" difficulty="Easy"/>
+              <DeckList name="Deck 1" difficulty="Easy"/>
+              <DeckList name="Deck 1" difficulty="Easy"/>
+              <DeckList name="Deck 1" difficulty="Easy"/>
+              <DeckList name="Deck 1" difficulty="Easy"/>
+              <DeckList name="Deck 1" difficulty="Easy"/>
+              <DeckList name="Deck 1" difficulty="Easy"/>
+              <DeckList name="Deck 1" difficulty="Easy"/>
+              <DeckList name="Deck 1" difficulty="Easy"/>
+              <DeckList name="Deck 1" difficulty="Easy"/>
+              <DeckList name="Deck 1" difficulty="Easy"/>
+            </ul>
+          </main>
+        </div>
       </div>
     </BackgroundAnim>
   )
