3d52df2a611584b3456c6435f8e1d11f2f3e3892
Jacky Yang <jackyyang1670@gmail.com>
2024-11-30 02:56:18 -0800

    Renamed to community pages for clarity

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,34 +5,37 @@ import './NavBar.css';
 function NavBar() {
   const navigate = useNavigate();
 
-  const navButtonStyle = {
-    backgroundColor: 'transparent',
-    padding: '0.8em 1.5em',
-    margin: '0 10px'
-  };
 
-  return (
-    <nav className="navbar">
-      <Button 
-        style={navButtonStyle} 
-        onClick={() => navigate('/share')}
-      >
-        Share
-      </Button>
-      <Button 
-        style={navButtonStyle} 
-        onClick={() => navigate('/decks')}
-      >
-        Decks
-      </Button>
-      <Button 
-        style={navButtonStyle} 
-        onClick={() => navigate('/friends')}
-      >
-        Friends
-      </Button>
-    </nav>
-  );
+  
+
+  // const navButtonStyle = {
+  //   backgroundColor: 'transparent',
+  //   padding: '0.8em 1.5em',
+  //   margin: '0 10px'
+  // };
+
+  // return (
+  //   <nav className="navbar">
+  //     <Button 
+  //       style={navButtonStyle} 
+  //       onClick={() => navigate('/share')}
+  //     >
+  //       Share
+  //     </Button>
+  //     <Button 
+  //       style={navButtonStyle} 
+  //       onClick={() => navigate('/decks')}
+  //     >
+  //       Decks
+  //     </Button>
+  //     <Button 
+  //       style={navButtonStyle} 
+  //       onClick={() => navigate('/friends')}
+  //     >
+  //       Friends
+  //     </Button>
+  //   </nav>
+  // );
 }
 
 export default NavBar;
---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import BackgroundAnim from '../components/BackgroundAnim'
+import './LandingPage.css'
+const LandingPage = () => {
+  return (
+    <BackgroundAnim color="teal" outlineColor="pink" title="Community" showTitle={false}>
+    </BackgroundAnim>
+  )
+}
+
+export default LandingPage
