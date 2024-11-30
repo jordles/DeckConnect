0022854e651300dd38698de7f1ba93c02004d80a
Jacky Yang <jackyyang1670@gmail.com>
2024-11-30 02:33:38 -0800

    Share page started

---
+++ frontend/src/pages/SharePage.jsx
@@ -0,0 +1,15 @@
+import BackgroundAnim from '../components/BackgroundAnim'
+import './SharePage.css'
+
+const SharePage = () => {
+  return (
+    <BackgroundAnim color="teal" outlineColor="pink" title="Community" >
+      <div className="share-page">
+        <header>
+        </header>
+      </div>
+    </BackgroundAnim>
+  )
+}
+
+export default SharePage
