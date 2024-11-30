00532a4db0b4adb2381ca8b1b97bc05d1ec3f71a
SuperWebMonkey <vikingsattack@outlook.com>
2024-11-30 12:42:46 -0800

    fixed styling that conflicted with the anim page

--- frontend/src/pages/CommunityPage.jsx
+++ frontend/src/pages/CommunityPage.jsx
@@ -1,15 +1,16 @@
-import BackgroundAnim from '../components/BackgroundAnim'
-import './CommunityPage.css'
+import BackgroundAnim from "../components/BackgroundAnim";
+import "./CommunityPage.css";
+import CommunityDeck from "./CommunityBoardPage.jsx";
 
 const CommunityPage = () => {
   return (
-    <BackgroundAnim color="teal" outlineColor="pink" title="Community" >
-      <div className="community-page">
-        <header>
-        </header>
-      </div>
+    <BackgroundAnim color="teal" outlineColor="pink" title="Community">
+      {/* <div className="community-page">
+        <header></header>
+      </div> */}
+      <CommunityDeck />
     </BackgroundAnim>
-  )
-}
+  );
+};
 
-export default CommunityPage
+export default CommunityPage;
