bfdf5022b6cfae116ab679d502ba921c17ca7ca6
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-11-30 13:09:35 -0700

    Modified logo component to link to decks page when clicked

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,11 +1,13 @@
 import './Logo.css'
+import { useNavigate } from 'react-router-dom'
 
 const Logo = ({style}) => {
+  const navigate = useNavigate();
+
   return (
-    <div className="logo" style={style}>
+    <div className="logo" style={style} onClick={() => navigate('/decks')} role="button" tabIndex={0}>
       <div className = "back"></div>
       <div className = "front">Deck <br/> Connect</div>
-      
     </div>
   )
 }
